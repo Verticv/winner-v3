40ddd05eec7dbbcf6f426bc190a3a87769a2982d
MohammedAlghazali <mohammed.m.alghazali@hotmail.com>
2023-03-21 13:00:29 +0200

    Add carousels for the main page

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,84 +1,133 @@
-import HotelCasino from 'components/mainPage/HotelCasino'
-import LiveCasino from 'components/mainPage/LiveCasino'
-import Other from 'components/mainPage/Other'
-import SlotGame from 'components/mainPage/SlotGame'
-import Sports from 'components/mainPage/Sports'
-import Tabs from 'components/mainPage/Tabs'
-import QuickMenu from 'components/QuickMenu'
-import React, { useEffect, useState } from 'react'
-import Carousel from '../components/mainPage/Carousel'
-import Footer from '../components/mainPage/Footer'
-import Navbar from '../components/mainPage/Navbar'
-import SubtitleLeft from '../images/subtitle_left.png'
-import SubtitleRight from '../images/subtitle_right.png'
+import Index from "components/mainPage/CarouselV2";
+import CarouselV2Vertical from "components/mainPage/CarouselV2Vertical";
+import CarouselV2WorkingExample from "components/mainPage/CarouselV2WorkingExample";
+import HotelCasino from "components/mainPage/HotelCasino";
+import LiveCasino from "components/mainPage/LiveCasino";
+import Other from "components/mainPage/Other";
+import SlotGame from "components/mainPage/SlotGame";
+import Sports from "components/mainPage/Sports";
+import Tabs from "components/mainPage/Tabs";
+import QuickMenu from "components/QuickMenu";
+import React, { useEffect, useState } from "react";
+import Carousel from "../components/mainPage/Carousel";
+import Footer from "../components/mainPage/Footer";
+import Navbar from "../components/mainPage/Navbar";
+import SubtitleLeft from "../images/subtitle_left.png";
+import SubtitleRight from "../images/subtitle_right.png";
 
 const MainPage = ({ isAuthenticated, setAuthenticated }) => {
+  function useWindowSize() {
+    // Initialize state with undefined width/height so server and client renders match
+    // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+    const [windowSize, setWindowSize] = useState({
+      width: undefined,
+      height: undefined,
+    });
+    useEffect(() => {
+      // Handler to call on window resize
+      function handleResize() {
+        // Set window width/height to state
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }
+      // Add event listener
+      window.addEventListener("resize", handleResize);
+      // Call handler right away so state gets updated with initial window size
+      handleResize();
+      // Remove event listener on cleanup
+      return () => window.removeEventListener("resize", handleResize);
+    }, []); // Empty array ensures that effect is only run on mount
+    return windowSize;
+  }
 
-	function useWindowSize() {
-		// Initialize state with undefined width/height so server and client renders match
-		// Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-		const [windowSize, setWindowSize] = useState({
-			width: undefined,
-			height: undefined,
-		});
-		useEffect(() => {
-			// Handler to call on window resize
-			function handleResize() {
-				// Set window width/height to state
-				setWindowSize({
-					width: window.innerWidth,
-					height: window.innerHeight,
-				});
-			}
-			// Add event listener
-			window.addEventListener("resize", handleResize);
-			// Call handler right away so state gets updated with initial window size
-			handleResize();
-			// Remove event listener on cleanup
-			return () => window.removeEventListener("resize", handleResize);
-		}, []); // Empty array ensures that effect is only run on mount
-		return windowSize;
-	}
+  const size = useWindowSize();
 
-	const size = useWindowSize();
+  const SubComp = ({ text }) => (
+    <div className="flex justify-center h-22px items-center space-x-29px flex-shrink-0">
+      <img
+        src={SubtitleLeft}
+        className="object-none flex-shrink-0 mt-3px"
+        alt=""
+      />
+      <h3
+        style={{ color: "#ad9e8c", fontSize: "24px" }}
+        className="font-spoqaBold h-22px flex items-center flex-shrink-0"
+      >
+        {text}
+      </h3>
+      <img
+        src={SubtitleRight}
+        className="object-none flex-shrink-0 mt-3px"
+        alt=""
+      />
+    </div>
+  );
 
-	const SubComp = ({ text }) => (
-		<div className='flex justify-center h-22px items-center space-x-29px flex-shrink-0'>
-			<img src={SubtitleLeft} className="object-none flex-shrink-0 mt-3px" alt="" />
-			<h3 style={{ color: '#ad9e8c', fontSize: '24px' }} className="font-spoqaBold h-22px flex items-center flex-shrink-0">{text}</h3>
-			<img src={SubtitleRight} className="object-none flex-shrink-0 mt-3px" alt="" />
-		</div>
-	)
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPosition(position);
+  };
 
-	const [scrollPosition, setScrollPosition] = useState(0);
-	const handleScroll = () => {
-		const position = window.pageYOffset;
-		setScrollPosition(position);
-	};
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-	useEffect(() => {
-		window.addEventListener('scroll', handleScroll, { passive: true });
-		return () => {
-			window.removeEventListener('scroll', handleScroll);
-		};
-	}, []);
+  return (
+    <div
+      style={{ backgroundColor: "#1e1e1e" }}
+      className="w-full relative flex flex-col justify-center items-start limit:items-center limit1920:overflow-x-hidden"
+    >
+      {/* <div className="fixed w-full top-0 z-50 flex flex-col items-start limit1920:items-center">
+        <Navbar isAuthenticated={isAuthenticated} setAuth={setAuthenticated} />
+      </div> */}
 
-	return (
-		<div style={{ backgroundColor: '#1e1e1e' }} className="w-full relative flex flex-col justify-center items-start limit:items-center limit1920:overflow-x-hidden">
-
-			<div className="fixed w-full top-0 z-50 flex flex-col items-start limit1920:items-center">
-				<Navbar isAuthenticated={isAuthenticated} setAuth={setAuthenticated} />
-			</div>
-
-			<div style={{ width: '1496px', height: 'calc(100vh - 497px)' }} className={`${scrollPosition > 497 ? "top-235px" : "bottom-0"} fixed z-20 flex justify-end`}>
+      {/* <div style={{ width: '1496px', height: 'calc(100vh - 497px)' }} className={`${scrollPosition > 497 ? "top-235px" : "bottom-0"} fixed z-20 flex justify-end`}>
 				<QuickMenu scrollPosition={scrollPosition} />
-			</div>
+			</div> */}
 
-			<div style={{marginLeft: size.width < 1261 ? '-324px' : '0px'}} className="flex items-center justify-start limit:justify-center w-screen">
+      <div
+        style={{
+          background: "green",
+          maxWidth: "300px",
+          marginTop: "20px",
+          marginBottom: "20px",
+        }}
+      >
+        <CarouselV2Vertical />
+      </div>
+
+      <div
+        style={{
+          background: "green",
+          maxWidth: "1260px",
+          marginTop: "250px",
+          marginBottom: "20px",
+        }}
+      >
+        <Index />
+      </div>
+      <div
+        style={{
+          background: "blue",
+          maxWidth: "1260px",
+          marginTop: "20px",
+          marginBottom: "250px",
+        }}
+      >
+        <CarouselV2WorkingExample />
+      </div>
+
+      {/* <div style={{marginLeft: size.width < 1261 ? '-324px' : '0px'}} className="flex items-center justify-start limit:justify-center w-screen">
 				<Carousel />
-			</div>
+			</div> */}
 
-			<div className="flex flex-col items-start limit:items-center h-full relative z-30">
+      {/* <div className="flex flex-col items-start limit:items-center h-full relative z-30">
 
 				<div style={{ marginTop: '31px' }}>
 					<Tabs />
@@ -121,12 +170,11 @@ const MainPage = ({ isAuthenticated, setAuthenticated }) => {
 				<div style={{ marginTop: '30px', marginBottom: '92px' }}>
 					<Other />
 				</div>
-			</div>
+			</div> */}
 
-			<Footer />
+      <Footer />
+    </div>
+  );
+};
 
-		</div>
-	)
-}
-
-export default MainPage
+export default MainPage;
