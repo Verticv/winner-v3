b0b21bbd65a67a0cd8814d07f5162ec0843020b9
Andrew Jang <dhj9817@gmail.com>
2023-08-09 22:10:16 +0900

    .

--- src/components/mainPage/NavBar/LinkButton.jsx
+++ src/components/mainPage/NavBar/LinkButton.jsx
@@ -5,7 +5,7 @@ const LinkButton = ({ buttonText, ButtonIcon, count, isAuthenticated }) => {
     <div
       style={{
         color: "#2d2834",
-        height: "81px",
+        height: "82px",
         marginTop: "-3px",
       }}
       className={`w-80px flex-shrink-0 text-r2d2834 relative flex flex-col items-center justify-center cursor-pointer hover:filter hover:brightness-150`}
@@ -16,7 +16,7 @@ const LinkButton = ({ buttonText, ButtonIcon, count, isAuthenticated }) => {
             className="absolute flex items-center justify-center px-8px h-19px top-2px -right-8px text-white font-roboto tracking-tight text-12px bg-rf04281 rounded-9px"
             style={{ width: count < 10 ? "19px" : "30px" }}
           >
-            <p className={`flex items-center justify-center text-white font-roboto tracking-tight text-12px h-10px`}>
+            <p className={`flex items-center justify-center text-white font-roboto tracking-tight text-12px h-10px -mt-px`}>
               {count}
             </p>
           </div>
@@ -26,7 +26,7 @@ const LinkButton = ({ buttonText, ButtonIcon, count, isAuthenticated }) => {
       </div>
       <span
         style={{ marginBottom: "0px", color: "#5e399a", fontSize:'13px' }}
-        className="cursor-pointer font-bold text-14px tracking-tight -mt-2px"
+        className="cursor-pointer font-bold text-14px tracking-tight -mt-px"
       >
         {buttonText}
       </span>
