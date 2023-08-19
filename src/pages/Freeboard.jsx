985a09a799f2ee9a237438de90aae4ca85dba2e2
ReemaAlghazali <reemaalghazali13@gmail.com>
2023-08-19 03:17:04 +0300

    Freeboard page

--- src/pages/Freeboard.jsx
+++ src/pages/Freeboard.jsx
@@ -1,14 +1,14 @@
-import Footer from 'components/mainPage/Footer';
-import QuickMenu from 'components/QuickMenu';
-import FreeBoardCompose from 'components/freeBoard/FreeBoardCompose';
-import FreeBoardMain from 'components/freeBoard/FreeBoardMain';
-import FreeBoardView from 'components/freeBoard/FreeBoardView';
-import DirectoryComponent from 'components/myPage/DirectoryComponent';
-import Navbar from '../components/mainPage/NavBar';
-import React, { useEffect, useState } from 'react';
-import { Route } from 'react-router';
-import FreeBoardBanner from '../images/freeBoard/free_board_banner_v2.png';
-import FreeBoardView2 from 'components/freeBoard/FreeBoardView2';
+import Footer from "components/mainPage/Footer";
+import QuickMenu from "components/QuickMenu";
+import FreeBoardCompose from "components/freeBoard/FreeBoardCompose";
+import FreeBoardMain from "components/freeBoard/FreeBoardMain";
+import FreeBoardView from "components/freeBoard/FreeBoardView";
+import DirectoryComponent from "components/myPage/DirectoryComponent";
+import Navbar from "../components/mainPage/NavBar";
+import React, { useEffect, useState } from "react";
+import { Route } from "react-router";
+import FreeBoardBanner from "../images/freeBoard/free_board_banner_v3.png";
+import FreeBoardView2 from "components/freeBoard/FreeBoardView2";
 
 const Freeboard = ({ isAuthenticated, setAuthenticated }) => {
   const [scrollPosition, setScrollPosition] = useState(0);
@@ -18,71 +18,76 @@ const Freeboard = ({ isAuthenticated, setAuthenticated }) => {
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll, { passive: true });
+    window.addEventListener("scroll", handleScroll, { passive: true });
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
   return (
-    <div className='relative flex flex-col justify-center items-center limit1920:overflow-x-hidden bg-gray-1e1e1e'>
-      <div className='fixed w-full top-0 z-50 flex flex-col items-start limit1920:items-center'>
+    <div
+      style={{ background: "linear-gradient(to right, #b644c4, #351894)" }}
+      className="relative flex flex-col justify-center items-center limit1920:overflow-x-hidden bg-gray-1e1e1e"
+    >
+      <div className="fixed w-full top-0 z-50 flex flex-col items-start limit1920:items-center">
         <Navbar isAuthenticated={isAuthenticated} setAuth={setAuthenticated} />
       </div>
       <div
         style={{
-          width: '1496px',
-          top: scrollPosition > 200 ? '235px' : '430px',
+          width: "1496px",
+          top: scrollPosition > 200 ? "235px" : "430px",
         }}
         className={`right-208px fixed z-20 flex justify-end`}
       >
         <QuickMenu scrollPosition={scrollPosition} />
       </div>
-      <div style={{marginTop:'104px'}} className='flex flex-col items-start limit:items-center w-full h-full'>
-        <Route exact path='/freeboard'>
-          <DirectoryComponent branch1='게시판' mainPath='/freeboard' />
+      <div
+        style={{ marginTop: "154px" }}
+        className="flex flex-col items-start limit:items-center w-full h-full"
+      >
+        <Route exact path="/freeboard">
+          <DirectoryComponent branch1="게시판" mainPath="/freeboard" />
         </Route>
-        <Route path='/freeboard/view'>
+        <Route path="/freeboard/view">
           <DirectoryComponent
-            branch1='게시판'
-            branch2='뷰'
-            mainPath='/freeboard/view'
+            branch1="게시판"
+            branch2="뷰"
+            mainPath="/freeboard/view"
           />
         </Route>
-        <Route path='/freeboard/view2'>
+        <Route path="/freeboard/view2">
           <DirectoryComponent
-            branch1='게시판'
-            branch2='뷰'
-            mainPath='/freeboard/view'
+            branch1="게시판"
+            branch2="뷰"
+            mainPath="/freeboard/view"
           />
         </Route>
-        <Route path='/freeboard/compose'>
+        <Route path="/freeboard/compose">
           <DirectoryComponent
-            branch1='게시판'
-            branch2='작성하기'
-            mainPath='/freeboard/compose'
+            branch1="게시판"
+            branch2="작성하기"
+            mainPath="/freeboard/compose"
           />
         </Route>
-        <div style={{height:'125px'}} className='relative w-default'>
-          <img className='z-10 mt-px' src={FreeBoardBanner} alt='' />
-          <div
-            className='font-spoqaMedium z-20 absolute top-0 text-28px w-full h-full flex items-center justify-center'
-            style={{ color: '#ffdfbd' }}
+        <div style={{ height: "125px" }} className="relative w-default">
+          <img className="z-10 mt-px" src={FreeBoardBanner} alt="" />
+          {/* <div
+            className="font-spoqaMedium z-20 absolute top-0 text-28px w-full h-full flex items-center justify-center"
+            style={{ color: "#ffdfbd" }}
           >
-            <span className='leading-none mt-2px'>게시판</span>
-          </div>
+            <span className="leading-none mt-2px">게시판</span>
+          </div> */}
         </div>
-
-        <div className='mt-20px w-default z-30'>
-          <Route exact path='/freeboard'>
+        <div className="mt-20px w-default z-30">
+          <Route exact path="/freeboard">
             <FreeBoardMain />
           </Route>
-          <Route path='/freeboard/view'>
+          <Route path="/freeboard/view">
             <FreeBoardView />
           </Route>
-          <Route path='/freeboard/view2'>
+          <Route path="/freeboard/view2">
             <FreeBoardView2 />
           </Route>
-          <Route path='/freeboard/compose'>
+          <Route path="/freeboard/compose">
             <FreeBoardCompose />
           </Route>
         </div>
