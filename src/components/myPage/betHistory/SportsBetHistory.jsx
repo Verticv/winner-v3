94be2bf6276093b01774f3927139114b9142faf7
Andrew Jang <dhj9817@gmail.com>
2023-08-27 00:46:45 +0900

    .

--- src/components/myPage/betHistory/SportsBetHistory.jsx
+++ src/components/myPage/betHistory/SportsBetHistory.jsx
@@ -29,7 +29,7 @@ const SportsBetHistory = ({
 
       <div
         style={{ width: "1040px" }}
-        className="h-full mt-20px rounded-4px ml-2px"
+        className="h-full mt-20px rounded-4px"
       >
         <div className="space-y-15px">
           <SportsBetHistoryPanel
