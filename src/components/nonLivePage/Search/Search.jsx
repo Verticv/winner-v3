599aa24db338a706b3babb7f6d647527d6568832
Andrew Jang <dhj9817@gmail.com>
2023-09-14 16:21:30 +0900

    Update Search.jsx

--- src/components/nonLivePage/Search/Search.jsx
+++ src/components/nonLivePage/Search/Search.jsx
@@ -147,7 +147,7 @@ const Search = () => {
         </div>
       </div>
       {showSearchContent && (
-        <div className="items-start mr-10px absolute z-10 ml-px">
+        <div className="items-start mr-10px absolute z-10">
           <div
             style={{
               background: "#956bde",
