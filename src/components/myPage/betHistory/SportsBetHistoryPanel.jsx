4d8dfe264835ad33beff68ab8c719f9e96e89bf6
Andrew Jang <dhj9817@gmail.com>
2023-09-18 23:43:31 +0900

    .

--- src/components/myPage/betHistory/SportsBetHistoryPanel.jsx
+++ src/components/myPage/betHistory/SportsBetHistoryPanel.jsx
@@ -449,7 +449,7 @@ const SportsBetHistoryPanel = ({
             style={{
               width: isAttached === true ? (isFullPage ? "90px" : "85px") : "99px",
               paddingLeft: isPopup ? "7px" : !isAttached ? "7px" : isFullPage ? "0px" : "0px",
-              paddingRight: isAttached && !isFullPage && "2px",
+              // paddingRight: isAttached && !isFullPage && "2px",
               marginLeft: isAttached === true ? (isFullPage ? "32px" : "40px") : "10px",
             }}
             className="h-full flex items-center justify-center mb-px pb-px"
