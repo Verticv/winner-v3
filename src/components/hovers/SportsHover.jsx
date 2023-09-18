17798ae8cee9a28976502c28d70b1006894d9c2b
Andrew Jang <dhj9817@gmail.com>
2023-09-18 16:12:19 +0900

    .

--- src/components/hovers/SportsHover.jsx
+++ src/components/hovers/SportsHover.jsx
@@ -67,7 +67,7 @@ const SportsHover = ({ selection }) => {
             }}
             className="absolute z-50 top-20px right-15px flex items-center justify-center h-28px text-white rounded-14px cursor-pointer font-spoqaMedium text-13px tracking-tighter"
           >
-            {item.imgText === "이용가이드" ? "설명보기" : "게임시작"}
+            {item.id > 0 ? "설명보기" : "게임시작"}
           </button>
         )}
         {isHover === item.id && <div className="w-full h-full bg-black opacity-60 z-10 rounded-6px"></div>}
